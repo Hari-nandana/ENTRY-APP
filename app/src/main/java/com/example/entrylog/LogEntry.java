8fc5595a6d07222dcd7c95ba26788a8066d2d86b
Hari-nandana <harinandana2004@gmail.com>
2024-06-29 21:12:28 +0530

    completed

--- app/src/main/java/com/example/entrylog/LogEntry.java
+++ app/src/main/java/com/example/entrylog/LogEntry.java
@@ -1,6 +1,7 @@
 package com.example.entrylog;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,16 +14,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LogEntry extends AppCompatActivity {
 
     EditText e1,e2,e3,e4;
     AppCompatButton b1,b2;
-
+     String apiurl="http://10.0.4.16:3000/api/students";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log_entry);
+
 
         b1=(AppCompatButton) findViewById(R.id.logout);
         e1=(EditText) findViewById(R.id.name);
@@ -32,12 +44,49 @@ public class LogEntry extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Value reading
                 String name=e1.getText().toString();
                 String admissionno=e2.getText().toString();
                 String system=e2.getText().toString();
                 String department =e2.getText().toString();
 
-                Toast.makeText(getApplicationContext(),name+" "+admissionno+" "+system+" "+department,Toast.LENGTH_LONG).show();
+                //Json Object creation
+                JSONObject student=new JSONObject();
+                try
+                {
+                    student.put("name",name);
+                    student.put("admission_number",admissionno);
+                    student.put("system_number",system);
+                    student.put("department",department);
+                }
+                catch(JSONException e)
+                {
+                    throw new RuntimeException(e) ;
+
+                }
+                //Json Object Request creation
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiurl,
+                        student,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(getApplicationContext(), "added successfully", Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(),"something went wrong",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                );
+                //request queue
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(jsonObjectRequest);
+
+
 
             }
         });
@@ -46,6 +95,11 @@ public class LogEntry extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                SharedPreferences preferences=getSharedPreferences("entrylog",MODE_PRIVATE);
+                SharedPreferences.Editor editor= preferences.edit();
+                editor.clear();
+                editor.apply();
                 Intent i=new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
 
--- app/src/main/java/com/example/entrylog/MainActivity.java
+++ app/src/main/java/com/example/entrylog/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.entrylog;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -26,25 +27,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences=getSharedPreferences("entrylog",MODE_PRIVATE);
+        String username=preferences.getString("user",null);
+        if(username!=null)
+        {
+            Intent i=new Intent(getApplicationContext(), LogEntry.class);
+            startActivity(i);
+        }
+
        e1=(EditText) findViewById(R.id.uname);
        e2=(EditText) findViewById(R.id.pass);
        b1=(AppCompatButton) findViewById(R.id.login);
 
+
+
        b1.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               String username=e1.getText().toString();
-               String password=e2.getText().toString();
-               if(username.equals("admin")&&password.equals("1234"))
-               {
-                   Intent i=new Intent(getApplicationContext(), LogEntry.class);
-                   startActivity(i);
+                 String username=e1.getText().toString();
+                   String password=e2.getText().toString();
+                   if(username.equals("admin")&&password.equals("1234"))
+                   {
+
+                       SharedPreferences preferences=getSharedPreferences("entrylog",MODE_PRIVATE);
+                       SharedPreferences.Editor editor= preferences.edit();
+                       editor.putString("user","admin");
+                       editor.apply();
+                       Intent i=new Intent(getApplicationContext(), LogEntry.class);
+                       startActivity(i);
+
+                   }
+                   else
+                   {
+                       Toast.makeText(getApplicationContext(),"InvalidPassword",Toast.LENGTH_LONG).show();
+                   }
                }
-               else
-               {
-                   Toast.makeText(getApplicationContext(),"InvalidPassword",Toast.LENGTH_LONG).show();
-               }
-           }
+
+
        });
 
     }
